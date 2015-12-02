4567cbed5b8526b82e7801b0ea5aab42007fc6d5
cvs4190 <cvs_4190@hotmail.com>
2015-12-02 16:49:27 +0100

    e
    e

--- src/modules/module09/nominas.php
+++ src/modules/module09/nominas.php
@@ -32,7 +32,7 @@ function dameEmpleados() {
    
 }
  
- print json_encode($empleados);
+ //print json_encode($empleados);
  
  
 $columnas = 5;
@@ -42,18 +42,16 @@ print "<table border=\"1\">\n";            // Abre la tabla
 print "  <caption>Tabla</caption>\n";      // Crea la leyenda <caption>
 print "  <tbody>\n";                       // Abre el cuerpo de tabla <tbody>
 
-print "    <tr>\n";                        // Abre la primera fila
-print "      <th></th>\n";                 // Crea la primera celda <th> de la primera fila (sin número)
-for ($j = 1; $j <= $columnas; $j++) {      // Bucle 1 se ejecuta tantas veces como columnas tenga la tabla
-    print "      <th>$j</th>\n";           // Crea las celdas <th> de la primera fila (con número)
-}   
-print "    </tr>\n";                       // Cierra la primera fila
+//print "    <tr>\n";                        // Abre la primera fila
+//print "      <th></th>\n";                 // Crea la primera celda <th> de la primera fila (sin número)
+  
+//print "    </tr>\n";                       // Cierra la primera fila
 
-for ($i = 1; $i <= $filas; $i++) {         // Bucle 2 (genera el resto de filas de la tabla)
+for ($i = 0; $i <= $filas; $i++) {         // Bucle 2 (genera el resto de filas de la tabla)
     print "    <tr>\n";                    // Abre la fila
-    print "      <th>$i</th>\n";           // Crea la primera celda <th> de cada fila (con número)
-    for ($j = 1; $j <= $columnas; $j++) {  // Bucle 3 se ejecuta tantas veces como columnas tenga la tabla
-        print "      <td>$empleados[$i]</td>\n";     // Crea el resto de celdas <td> de cada fila (con números)
+    $aux = $empleados[$i];           // Crea la primera celda <th> de cada fila (con número)
+    for ($j = 0; $j <= $columnas; $j++) {  // Bucle 3 se ejecuta tantas veces como columnas tenga la tabla
+        print "      <td>$aux[$j]</td>\n";     // Crea el resto de celdas <td> de cada fila (con números)
     } 
     print "    </tr>\n";                   // Cierra la fila
 }
