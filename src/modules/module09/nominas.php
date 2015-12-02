9212a986575cf2373aeabf46861a2ec28869f602
cvs4190 <cvs_4190@hotmail.com>
2015-12-02 16:19:34 +0100

    php+html
    nuevo

--- src/modules/module09/nominas.php
+++ src/modules/module09/nominas.php
@@ -15,6 +15,13 @@
  */
 //put your code here  
 $empleados = array();
+
+$e1 = ["Pedro", "Lopez", 1, FALSE, 1000.0];
+    $e2 = ["Jonny", "Mentero", 2, TRUE, 1200.0];
+    $e3 = ["Armando", "Casas", 1, FALSE, 1050.0];
+    $e4 = ["Sole", "Dolio", 1, FALSE, 1000.0];
+    $e5 = ["Encarna", "Vales", 1, TRUE, 1150.0];
+    $empleados=[$e1,$e2,$e3,$e4,$e5];
 function dameEmpleados() {
     $e1 = ["Pedro", "Lopez", 1, FALSE, 1000.0];
     $e2 = ["Jonny", "Mentero", 2, TRUE, 1200.0];
@@ -22,6 +29,43 @@ function dameEmpleados() {
     $e4 = ["Sole", "Dolio", 1, FALSE, 1000.0];
     $e5 = ["Encarna", "Vales", 1, TRUE, 1150.0];
     $empleados=[$e1,$e2,$e3,$e4,$e5];
+   
+}
+ for ($i=0; $i<5; $i++) {
+    print ($empleados[$i]);
+}
+ print json_encode($empleados);
+ 
+ 
+$columnas = 4;
+$filas    = 3;
+
+print "<table border=\"1\">\n";            // Abre la tabla
+print "  <caption>Tabla</caption>\n";      // Crea la leyenda <caption>
+print "  <tbody>\n";                       // Abre el cuerpo de tabla <tbody>
+
+print "    <tr>\n";                        // Abre la primera fila
+print "      <th></th>\n";                 // Crea la primera celda <th> de la primera fila (sin número)
+for ($j = 1; $j <= $columnas; $j++) {      // Bucle 1 se ejecuta tantas veces como columnas tenga la tabla
+    print "      <th>$j</th>\n";           // Crea las celdas <th> de la primera fila (con número)
+}   
+print "    </tr>\n";                       // Cierra la primera fila
+
+for ($i = 1; $i <= $filas; $i++) {         // Bucle 2 (genera el resto de filas de la tabla)
+    print "    <tr>\n";                    // Abre la fila
+    print "      <th>$i</th>\n";           // Crea la primera celda <th> de cada fila (con número)
+    for ($j = 1; $j <= $columnas; $j++) {  // Bucle 3 se ejecuta tantas veces como columnas tenga la tabla
+        print "      <td>$i-$j</td>\n";     // Crea el resto de celdas <td> de cada fila (con números)
+    } 
+    print "    </tr>\n";                   // Cierra la fila
 }
 
-print json_encode($empleados);
+print "  </tbody>\n";                      // Cierra el cuerpo de tabla <tbody>
+print "</table>\n";     
+ 
+ 
+ 
+ 
+ 
+ 
+ ?>
